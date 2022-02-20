30e7b9400014437721459a84cfdcb26bcbfc121a
Marco Ochse <t3chn0m4g3@users.noreply.github.com>
2022-02-20 23:40:14 +0100

    make proxy_pass work via wss://

--- static/map.js
+++ static/map.js
@@ -4,7 +4,8 @@
 // - AttackMapServer machine:
 //   - Internal IP: 127.0.0.1
 //   - External IP: 192.168.11.106
-const WS_HOST = 'ws://'+window.location.host+'/websocket'
+// For Proxy_Pass to work we need to use wss:// instead of ws://
+const WS_HOST = 'wss://'+window.location.host+'/websocket'
 var webSock = new WebSocket(WS_HOST); // Internal
 
 // link map
